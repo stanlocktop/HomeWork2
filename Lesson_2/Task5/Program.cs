878dd3e2b471e5efda57bdf37364ccc8ad3c901e
stanlocktop <wlad-test@ukr.net>
2023-09-03 20:29:09 +0300

    Finish last task

--- Lesson_2/Task5/Program.cs
+++ Lesson_2/Task5/Program.cs
@@ -5,7 +5,22 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Введіть перше число: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Введіть друге число: ");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("До обміну:");
+            Console.WriteLine("Перше число: " + number1);
+            Console.WriteLine("Друге число: " + number2);
+
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+            Console.WriteLine("Результат:");
+            Console.WriteLine("Перше число: " + number1);
+            Console.WriteLine("Друге число: " + number2);
+            Console.ReadKey();
         }
     }
 }
