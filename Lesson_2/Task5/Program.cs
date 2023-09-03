bd44da4414932fddf64cd9d7b785f92681006b37
stanlocktop <wlad-test@ukr.net>
2023-09-03 17:28:52 +0300

    Add five task

---
+++ Lesson_2/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SwappingValues
+{
+    // Поміняти місцями значення двох змінних? Чи можна це зробити без ще однієї тимчасової змінної.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
