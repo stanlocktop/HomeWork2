e84e256fc036dde8ed3e491fb03fadb6b96c52b4
stanlocktop <wlad-test@ukr.net>
2023-09-03 17:20:50 +0300

    1-3 Tasks

---
+++ Lesson_2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2
+
+{
+    //2) Написати програму, яка обчислює квадрат будь-якого введеного числа.
+    public class Example
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть число, яке хочете обчислити під квадратом: ");
+            double num = double.Parse(Console.ReadLine());
+            double pow = Math.Pow(num, 2);
+            Console.WriteLine("Результат: ");
+            Console.WriteLine(pow);
+            Console.ReadKey();
+        }
+    }
+}
