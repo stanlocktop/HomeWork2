b985ce4157440c7e27f7e6353837f0ce8b2c7933
stanlocktop <wlad-test@ukr.net>
2023-09-03 17:48:19 +0300

    finish 4 task

--- Lesson_2/Task4/Program.cs
+++ Lesson_2/Task4/Program.cs
@@ -1,11 +1,19 @@
 ﻿namespace SwappingValues
 {
-    // Поміняти місцями значення двох змінних? Чи можна це зробити без ще однієї тимчасової змінної.
+    //4) Написати програму, яка обчислює середнє арифметичне двох чисел.
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть перше число: ");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть друге число: ");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+            double avg = (num1 + num2) / 2;
+            Console.WriteLine("Середнє арифметичне двох чисел: " + avg);
+            Console.ReadKey();
+
         }
     }
 }
